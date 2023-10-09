1e42d3333f66fa70a4d5fde8ac515fb43fe7855f
luanntpd03043 <thanhluanxhxh2000@gmail.com>
2023-10-09 16:40:26 +0700

    update

---
+++ DiceGame_vueJs/src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="app">
+    <div class="wrapper clearfix">
+
+        <layers
+            v-bind:scorePlayer="scorePlayer"
+            v-bind:currentScore="currentScore"
+            v-bind:activePlayer="activePlayer"
+        />
+
+        <controls
+        v-on:handleRollDice="handleRollDice"
+        v-on:handleNewGame="handleNewGame"
+        />
+
+        <dices
+         v-bind:dices="dices"
+        />
+
+        <popup
+        v-on:handleConfirm="handleConfirm"
+        v-bind:isOpenPopup="isOpenPopup"
+        />
+    </div>
+  </div>
+</template>
+
+<script>
+import Layers from './components/Players.vue';
+import Controls from './components/Controls.vue';
+import Dices from './components/Dices.vue';
+import Popup from './components/Popup.vue';
+export default {
+  name: "app",
+  data() {
+    return {
+        isPlaying: false,
+        isOpenPopup: false,
+        activePlayer: 0, //Người chơi hiện tại
+        currentScore: 30,
+        scorePlayer: [13,30],
+        dices: [1,5]
+    }
+  },
+  components: {
+    Layers,
+    Controls,
+    Dices,
+    Popup
+  },
+  methods:{
+    nextPlayer(){
+        this.activePlayer = this.activePlayer === 0 ? 1 : 0;
+        this.currentScore = 0;
+    },
+
+    handleRollDice(){
+        if(this.isPlaying){
+           var dice1 = Math.floor(Math.random() * 6) + 1;
+           var dice2 = Math.floor(Math.random() * 6) + 1;
+
+           if(dice1 === 1 || dice2 === 1){
+            let activePlayer = this.activePlayer;
+            setTimeout(() => {
+                alert(`Nguoi choi Player ${activePlayer + 1} da quay trung so 1.Rat tiec!`)
+            }, 10);
+            this.nextPlayer();
+           }else{
+               this.dices=[dice1,dice2];
+               this.currentScore = this.currentScore + dice1 + dice2;
+           }
+
+        }else{
+            alert('Vui lòng click NewGame!')
+        }
+    },
+
+    handleNewGame(){
+        this.isOpenPopup = true;
+    },
+
+    handleConfirm(){
+        this.isPlaying = true,
+        this.activePlayer = 0,
+        this.scorePlayer = [0,0],
+        this.currentScore = 0,
+        this.dices=[1,1],
+        this.isOpenPopup =  false
+    },
+
+
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.clearfix::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+body {
+  background-image: linear-gradient(
+      rgba(62, 20, 20, 0.4),
+      rgba(62, 20, 20, 0.4)
+    ),
+    url(/public/assets/img/back.jpg);
+  background-size: cover;
+  background-position: center;
+  font-family: Lato;
+  font-weight: 300;
+  position: relative;
+  height: 100vh;
+  color: #555;
+}
+
+.wrapper {
+  width: 1000px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+}
+
+</style>
---
+++ DiceGame_vueJs/src/components/Popup.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="popup" v-bind:class="getClassPopup">
+    <div class="popup-inner">
+      <h3>Luật chơi</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, iste
+        magni corporis ducimus, aliquid repellendus architecto ipsum aliquam
+        consectetur odio quisquam eaque doloribus iusto perspiciatis ratione
+        itaque reprehenderit distinctio vel?
+      </p>
+      <button 
+      v-on:click="confirm"
+      class="confirm">Đã hiểu</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "popup",
+  props:{
+    isOpenPopup: {
+        type: Boolean,
+        default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  methods:{
+    confirm(){
+        this.$emit('handleConfirm')
+    }
+  },
+  computed:{
+    getClassPopup: function(){
+        return{
+            'open': this.isOpenPopup
+        }
+    },
+  }
+};
+</script>
+
+<style>
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  pointer-events: none;
+  opacity: 0;
+  transition: opacity .3s ease;
+}
+.popup.open {
+    opacity: 1;
+    pointer-events: auto;
+}
+.popup.open .popup-inner{
+    transform: translate(-50%,-50%) scale(1);
+}
+
+.popup-inner{
+    position: absolute;
+    background-color: #fff;
+    max-width: 400px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%) scale(.7);
+    z-index: 5;
+    padding: 10px;
+    transition: transform .3s ease;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.popup-inner h3{
+    font-weight: bold;
+    margin-bottom: 10px;
+    font-size: 20px;
+    color: #000;
+}
+
+.popup-inner p{
+    font-size: 16px;
+}
+
+.popup .confirm{
+    border: 2px solid #333;
+    padding: 8px 15px;
+    background-color: #fff;
+    margin-top: 20px;
+    cursor: pointer;
+    outline: none;
+    transition: .3s ease-in-out;
+    font-size: 16px;
+}
+.popup .confirm:hover{
+    background-color: #000;
+    color: #fff;
+}
+
+/* Kết hợp css3 vs VueJs để cây dựng hiệu ứng - Zoom in - Zoom Out */
+
+</style>
